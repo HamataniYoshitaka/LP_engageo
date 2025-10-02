f434a91203bdc42a87de2dc82bebcf3c3a90fe07
MafuyuKinoshita <>
2025-10-02 11:05:52 +0900

    フォルダ作成

---
+++ index.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./src/output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Poppins:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="relative z-50 w-full h-16 bg-white px-20 flex justify-between items-center">
+        <img src="./src/images/icon.svg" alt="" class="w-40 h-auto">
+        <button class="text-white bg-sunshade-400 rounded-full px-6 py-2 text-base hover:brightness-110 transition-all duration-300 ">すぐに無料簡易診断</button>
+    </div>
+    <div class="bg-[#F2F9FF] relative overflow-hidden">
+        <div class="flex justify-end">
+            <div class="relative">
+                <div class="absolute bottom-0 left-0 w-40 h-full bg-gradient-to-r from-[#F2F9FF] to-transparent "></div>
+                <img src="./src/images/main-photo.webp" alt="" class="w-[850px] h-auto "/>
+            </div>
+        </div>
+        <div class="absolute top-1/2 -translate-y-1/2 left-24">
+            <div class="relative inline-flex flex-col justify-center gap-4 tracking-wider">
+                <p class="text-3xl"><span class="text-emphasis-dot">社員の離職</span>にお悩みの企業様へ</p>
+                <p class="text-4xl bg-white text-tradewind-400 p-1">人材定着力<span class="text-xl">（離職防止力）</span>を高めるため</p>
+                <div class="flex items-end gap-4"><p class="text-6xl bg-tradewind-400 text-white p-2">人財定着力診断&reg;</p><p class="text-3xl">で</p></div>
+                <p class="text-3xl">組織の長所・短所を見える化しませんか?</p>
+                <button class="text-white bg-sunshade-400 rounded-full pl-16 pr-6 py-4 text-2xl w-max flex items-center mt-12 shadow-lg hover:translate-x-1 hover:translate-y-1 hover:shadow-md transition-all duration-300">
+                    <p>すぐに無料簡易診断</p>
+                    <svg class="w-6 h-6 text-white mt-1 ml-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="currentColor" d="M471.1 297.4C483.6 309.9 483.6 330.2 471.1 342.7L279.1 534.7C266.6 547.2 246.3 547.2 233.8 534.7C221.3 522.2 221.3 501.9 233.8 489.4L403.2 320L233.9 150.6C221.4 138.1 221.4 117.8 233.9 105.3C246.4 92.8 266.7 92.8 279.2 105.3L471.2 297.3z"/></svg>
+                </button>
+            </div>
+        </div>
+        <svg class="absolute -top-10 left-0" width="144" height="145" viewBox="0 0 144 145" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle opacity="0.6" cx="48.5588" cy="65.5588" r="68.5588" fill="white"/>
+            <circle opacity="0.2" cx="106.235" cy="108" r="37" fill="#F3E15C"/>
+        </svg>
+        <svg class="absolute -top-10 left-80" width="104" height="104" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle opacity="0.8" cx="51.75" cy="51.75" r="51.75" fill="white"/>
+        </svg>
+        <svg class="absolute top-5 right-96" width="72" height="71" viewBox="0 0 72 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <ellipse opacity="0.3" cx="36" cy="35.25" rx="36" ry="35.25" fill="#F3E15C"/>
+        </svg>
+        <svg class="absolute -top-5 right-0" width="157" height="181" viewBox="0 0 157 181" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle opacity="0.5" cx="127.5" cy="56.5" r="67.5" fill="#F3E15C"/>
+            <circle opacity="0.4" cx="67.5" cy="113.5" r="67.5" fill="white"/>
+        </svg>
+        <svg class="absolute bottom-0 left-8" width="120" height="101" viewBox="0 0 120 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle opacity="0.5" cx="60" cy="60" r="60" fill="white"/>
+        </svg>
+        <svg class="absolute bottom-2 left-[520px]" width="195" height="192" viewBox="0 0 195 192" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle opacity="0.5" cx="67.5005" cy="124.5" r="67.5" transform="rotate(-180 67.5005 124.5)" fill="#F3E15C"/>
+            <circle opacity="0.4" cx="127.5" cy="67.5" r="67.5" transform="rotate(-180 127.5 67.5)" fill="white"/>
+        </svg>
+        <svg class="absolute bottom-0 right-5" width="120" height="72" viewBox="0 0 120 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle opacity="0.3" cx="60" cy="60" r="60" fill="#F3E15C"/>
+        </svg>
+    </div>
+    <div class="bg-white w-full">
+        <div class="flex justify-center items-center gap-11 pt-11 pb-48">
+            <img src="./src/images/pr1.svg" alt="" class="w-64 h-auto">
+            <img src="./src/images/pr1.svg" alt="" class="w-64 h-auto">
+            <img src="./src/images/pr1.svg" alt="" class="w-64 h-auto">
+        </div>
+    </div>
+    <div class="relative bg-[#F5F8F5] w-full h-[400px]">
+        <div class="absolute -top-28 left-1/2 -translate-x-1/2 w-[1039px] h-auto mx-auto bg-white shadow-2xl p-9 flex justify-between items-center">
+            <div class="text-center">
+                <p class="text-3xl">
+                    まずは
+                    <span class="relative text-4xl text-white bg-[#C49D58] px-2 py-1 rounded-md">
+                        <svg class="absolute -top-4 -left-4" width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.74414 6.16211L13.0456 11.7977" stroke="#C49D58" stroke-width="1.79074" stroke-linecap="round"/>
+                            <path d="M16.1483 1.97023L17.111 7.53388" stroke="#C49D58" stroke-width="1.79074" stroke-linecap="round"/>
+                            <path d="M2.22687 15.0661L7.72136 16.3668" stroke="#C49D58" stroke-width="1.79074" stroke-linecap="round"/>
+                        </svg>
+                        無料</span>
+                    の組織傾向診断で
+                </p>
+                <p class="text-tradewind-400 text-4xl pt-3">自社の特徴をチェック!</p>
+                <div class="flex justify-center items-center gap-2 pt-5">
+                    <p class="w-32 h-32 border border-tradewind-400 rounded-full flex justify-center items-center text-2xl text-tradewind-400">自社の<br/>組織<br/>タイプ</p>
+                    <p class="w-32 h-32 border border-tradewind-400 rounded-full flex justify-center items-center text-2xl text-tradewind-400">有効な<br/>改善策</p>
+                    <div class=" w-32 h-32 border border-tradewind-400 rounded-full flex justify-center items-center text-2xl text-tradewind-400">
+                        <p>簡単<br/>
+                        <span class="relative text-3xl marker">
+                            <svg class="absolute -top-1 -left-3" width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.09277 4.89551L10.1717 9.23148" stroke="#45B3B1" stroke-width="1.37778" stroke-linecap="round"/>
+                                <path d="M12.559 1.66807L13.2997 5.94868" stroke="#45B3B1" stroke-width="1.37778" stroke-linecap="round"/>
+                                <path d="M1.84786 11.7443L6.07525 12.745" stroke="#45B3B1" stroke-width="1.37778" stroke-linecap="round"/>
+                            </svg>
+                        3分</span>診断</p>
+                    </div>
+                </div>
+            </div>
+            <img src="./src/images/materials-1.svg" alt="" class="w-[457px] h-auto">
+        </div>
+    </div>
+    <div class="w-full bg-gradient-to-b from-white to-[#F2F9FF]">
+        <div class="flex justify-center">
+            <div class="relative w-max">
+                <img src="./src/images/what.svg" alt="" class="absolute top-10 -left-14"/>
+                <img src="./src/images/icon.svg" alt="" class="w-80 h-auto mt-24"/>
+            </div>
+        </div>
+        <div class="flex justify-center gap-10">
+            <div class="pt-12">
+                <p class="text-4xl text-tradewind-400">人材の定着力を高める</p>
+                <p class="text-xl font-normal pt-6 leading-relaxed">
+                    組織の傾向や個性を数字で表し<span class="text-tradewind-400 font-bold">「見える化」</span>し<br/>
+                    組織をイキイキさせるための組織特性診断です<br/>
+                    長所はさらに伸ばして、短所は改善することで、
+                </p>
+                <p class="w-max text-3xl text-tradewind-400 marker pt-4">既存メンバーがイキイキ働き、</p>
+                <p class="w-max text-3xl text-tradewind-400 marker pt-1">人材の定着率を高める</p>
+            </div>
+            <img src="./src/images/materials-2.svg" alt="" class="w-[597px] h-auto">
+        </div>
+    </div>
+    <div class="bg-gradient-to-b from-[#DEE1E2] to-white">
+        <div class="flex flex-col items-center pt-16">
+            <p class="text-3xl text-tradewind-400 poppins">PROBLEM</p>
+            <div class="border border-tradewind-400 w-6 rounded-full my-7"></div>
+            <p class="text-3xl">
+                <span class="relative text-white bg-[#C49D58] px-2 py-1 rounded-md mr-2">
+                    <svg class="absolute -top-6 -left-4" width="15" height="21" viewBox="0 0 15 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.9798 18.2933C9.32123 18.2933 7.74356 17.0219 7.51923 15.5043C7.47878 15.236 7.46407 14.8355 7.59646 14.3835C6.43803 14.3431 5.36786 14.016 4.52202 13.1929C3.86374 12.5499 3.42979 11.7782 3.25326 10.9624C3.02893 10.8889 2.80828 10.8044 2.5913 10.7052C0.314898 9.69101 -0.291898 7.62954 0.119988 6.03475C0.56865 4.29665 1.83373 2.78638 3.68722 1.77953C5.6731 0.702862 8.0966 0.346423 9.71473 0.890268C10.2774 1.08135 10.579 1.68766 10.3877 2.24988C10.1965 2.8121 9.58601 3.11342 9.02703 2.92234C8.12603 2.61734 6.35344 2.77535 4.71325 3.66461C3.94832 4.07985 2.60234 5.00953 2.20148 6.5639C2.02496 7.25473 2.25297 8.17706 3.41875 8.71723C3.7718 7.77653 4.58086 6.68516 6.33873 5.80692C7.64059 5.15651 9.23665 4.97278 10.3105 5.35127C11.0313 5.60482 11.3623 6.04577 11.5131 6.37282C12.5575 8.62904 9.7515 10.0511 8.69237 10.5876C7.77666 11.0506 6.76165 11.2968 5.73561 11.3262C5.8202 11.4401 5.91581 11.5504 6.02246 11.6533C6.81314 12.4213 8.43494 12.4176 11.1269 11.6422C11.6969 11.4806 12.2927 11.8076 12.4582 12.3772C12.6237 12.9467 12.2927 13.542 11.7227 13.7074C11.7227 13.7074 11.719 13.7074 11.7153 13.7074C10.1965 14.1483 9.57866 14.7693 9.64118 15.1883C9.70737 15.6255 10.3252 16.162 10.9872 16.1437C11.5645 16.129 12.0684 16.6103 12.0757 17.2056C12.0831 17.7972 11.6087 18.286 11.0129 18.2933C11.0019 18.2933 10.9872 18.2933 10.9761 18.2933H10.9798ZM9.05277 7.30985C8.59307 7.30985 7.94582 7.41274 7.29857 7.7361C6.48584 8.14031 5.89743 8.64006 5.58116 9.18023C6.27622 9.18023 7.0154 9.03692 7.72149 8.67681C9.25136 7.90146 9.5235 7.49358 9.5713 7.37232C9.4573 7.33557 9.2771 7.30985 9.05277 7.30985Z" fill="#C49D58"/>
+                        <path d="M13.0724 20.8363C12.7892 20.8363 12.5134 20.7224 12.3111 20.5203C12.1089 20.3182 11.9949 20.0426 11.9949 19.7596C11.9949 19.6898 11.9986 19.6163 12.0133 19.5502C12.028 19.4841 12.05 19.4106 12.0758 19.3481C12.1052 19.2819 12.1383 19.2195 12.1751 19.1607C12.2118 19.1019 12.256 19.0468 12.3075 18.999C12.3553 18.9476 12.4141 18.9035 12.4693 18.8667C12.5281 18.83 12.587 18.7969 12.6568 18.7675C12.7194 18.7454 12.7892 18.7197 12.8591 18.705C13.2085 18.6389 13.5799 18.7528 13.83 19.0027C13.8778 19.0504 13.9256 19.1092 13.9624 19.1644C13.9991 19.2195 14.0322 19.2819 14.0617 19.3518C14.0837 19.4142 14.1095 19.4841 14.1242 19.5539C14.1389 19.62 14.1426 19.6935 14.1426 19.7633C14.1426 20.0463 14.0286 20.3219 13.8263 20.524C13.624 20.7261 13.3482 20.84 13.065 20.84L13.0724 20.8363Z" fill="#C49D58"/>
+                    </svg>
+                    離職の多さ</span>経営幹部・人事部の皆様
+            </p>
+            <p class="text-3xl pt-2">このように思っていませんか?</p>
+        </div>
+        <div class="w-[1040px] mx-auto bg-white rounded-2xl shadow-2xl grid grid-cols-3 divide-x divide-dashed divide-[#D9D9D9] pt-14 pb-11 mt-8 ">
+            <div class="flex flex-col justify-between items-center gap-5">
+                <p class="text-2xl">離職を防止するためには<br/><span class="text-tradewind-400 marker">給料を上げるしかない</span></p>
+                <img src="./src/images/problem-1.svg" alt="" class="w-[150px] h-auto">
+            </div>
+            <div class="flex flex-col justify-between items-center gap-5">
+                <p class="text-2xl">離職よりもまずは<br/><span class="text-tradewind-400 marker">採用力を上げたい</span></p>
+                <img src="./src/images/problem-2.svg" alt="" class="w-[121px] h-auto">
+            </div>
+            <div class="flex flex-col justify-between items-center gap-5">
+                <p class="text-2xl">離職は仕方がない<br/><span class="text-tradewind-400 marker">どうにもならない</span></p>
+                <img src="./src/images/problem-3.svg" alt="" class="w-[140px] h-auto">
+            </div>
+        </div>
+        <div class="flex flex-col items-center pt-16 pb-10">
+            <p class="text-3xl text-tradewind-400">そんな皆様へご提案です</p>
+            <svg class="mt-5" width="74" height="47" viewBox="0 0 74 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M36.9195 46.7988L73.3842 0.557644L0.454826 0.557644L36.9195 46.7988Z" fill="url(#paint0_linear_2208_2659)"/>
+                <defs>
+                <linearGradient id="paint0_linear_2208_2659" x1="36.9195" y1="-14.8561" x2="36.9195" y2="46.7988" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#68D2CF" stop-opacity="0"/>
+                <stop offset="1" stop-color="#51CCC8"/>
+                </linearGradient>
+                </defs>
+            </svg>
+        </div>
+    </div>
+    <div class="bg-[#F5F8F5]">
+        <div class="flex flex-col items-center pt-16">
+            <p class="text-3xl text-tradewind-400 poppins">FEATURES</p>
+            <div class="border border-tradewind-400 w-6 rounded-full my-7"></div>
+            <p class="text-3xl">Engageoが選ばれる<span class="text-6xl text-tradewind-400 poppins">5</span>つの理由</p>
+        </div>
+        <div class="flex pt-36">
+            <div>
+                <p class="text-2xl text-white bg-tradewind-400 px-2 py-1 rounded-3xl poppins">Point.1</p>
+            </div>
+            <img src="./src/images/point-1.svg" alt="" class="w-[503px] h-auto">
+        </div>
+
+    </div>
+
+
+
+
+
+
+</body>
+</html>
